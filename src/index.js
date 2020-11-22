c30186d16d5c67e650aed463f0e0e5cc7abcabb2
NgocAnh <ngoc.anh@jvb-corp.com>
2020-11-22 23:54:11 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const port = 3000
+const exphbs  = require('express-handlebars');
+const path = require('path')
+const sass = require('node-sass');
+
+
+app.use(express.static(path.join(__dirname,'public')))
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+
+app.engine('hbs', exphbs({extname: '.hbs'}));
+
+app.set('views', path.join(__dirname,'resources','views'));
+
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
